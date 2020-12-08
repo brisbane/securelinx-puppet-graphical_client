a8c619e02b1943d3e7b43e2e5926a86395cf6fb8
Sean Brisbane <seancbrisbane@hotmail.com>
2020-12-08 12:21:23 +0000

    scrollbars were missing without mate themes asd default

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,13 +37,20 @@
 class graphical_client ( $disable_lock_screens = false ) {
    
    # kernel for nvidia
-   ensure_packages (["xcb-util-keysyms.i686", "xcb-util-keysyms", "kernel-devel"])
+   ensure_packages (["xcb-util-keysyms.i686", "xcb-util-keysyms", "kernel-devel", "dconf-editor"])
+	exec { 'dconfupdate' :
+	    command     => '/usr/bin/dconf update',
+	    refreshonly => true,
+	}
+
+  
    #Note, module does not support adding new RHEL6 only maintaining
    if $facts['os']['release']['major'] != '6'
    {
+
        yumgroup { "graphical-server-environment": ensure => "present"}
-       ensure_packages (['dconf.x86_64', 'dconf.i686'])
-       
+       ensure_packages (['dconf.x86_64', 'dconf.i686'])    
+       ensure_packages (['xorg-x11-fonts-ISO8859-1-75dpi', 'xorg-x11-fonts-misc', 'xorg-x11-fonts-ISO8859-1-100dpi', 'xorg-x11-fonts-100dpi', 'ucs-miscfixed-fonts', 'vlgothic-fonts', 'dejavu-serif-fonts', 'dejavu-sans-mono-fonts', 'xorg-x11-apps'] )
 
 
 	#doesnt work runlevel { 'graphical': persist => true }
@@ -54,10 +61,6 @@ class graphical_client ( $disable_lock_screens = false ) {
 	else {
 	  $ensure_lock_suppression   = "absent"
 	  $ensure_shield_suppression = "absent"
-	}
-	exec { 'dconfupdate' :
-	    command     => '/usr/bin/dconf update',
-	    refreshonly => true,
 	}
 	file {'/etc/dconf/db/local.d/02-animations':
 	   source => "puppet:///modules/$name/dconf-db-02-animations",
@@ -87,6 +90,15 @@ class graphical_client ( $disable_lock_screens = false ) {
 	}     
 	   
    #From https://www.reddit.com/r/Puppet/comments/455gst/using_puppet_to_do_a_dconf_load/
-   }
+
+    #scrollbars in reveal requre TraditionalOK theme here
+    ensure_packages (["mate-themes"])
+    file {'/etc/dconf/db/local.d/04-mate-themes':
+      source => "puppet:///modules/$name/dconf-db-04-mate-themes",
+      mode   => "0644",
+      notify => Exec['dconfupdate'],
+      ensure => "$ensure_lock_suppression"
+    }
+  }#!RHEL6
 }
 
